96db8ee90f4291cbcb38c256b9fb36a4c772750e
Bruce Ediger <bediger8@gmail.com>
2021-09-06 14:25:39 -0600

    X-wing rows, eliminates from columns.
    This seems to work.

--- board/xwing.go
+++ board/xwing.go
@@ -3,62 +3,73 @@ package board
 import "fmt"
 
 func (bd *Board) XwingEliminate(announce bool) int {
+	eliminated := bd.xwingRowEliminate(announce)
+	eliminated += bd.xwingColEliminate(announce)
+	return eliminated
+}
+
+// xwingRowEliminate finds 2 rows with some possible value in the same columns,
+// eliminates that possible value from the columns.
+func (bd *Board) xwingRowEliminate(announce bool) int {
+
+	var colsForRow [9]map[int][]int
+
+	for idx := range colsForRow {
+		colsForRow[idx] = make(map[int][]int)
+	}
 
 	for rowNo := 0; rowNo < 9; rowNo++ {
-		// find cells in row with only 2 possibilities
-		cellsWith := make(map[int][]*pairMarker)
 		for colNo := 0; colNo < 9; colNo++ {
-			if len(bd[rowNo][colNo].Possible) == 2 {
-				rep := 10*bd[rowNo][colNo].Possible[0] + bd[rowNo][colNo].Possible[1]
-				cellsWith[rep] = append(cellsWith[rep],
-					&pairMarker{
-						row: rowNo,
-						col: colNo,
-						a:   bd[rowNo][colNo].Possible[0],
-						b:   bd[rowNo][colNo].Possible[1],
-						rep: rep,
-					},
-				)
+			if bd[rowNo][colNo].Solved {
+				continue
+			}
+			for _, poss := range bd[rowNo][colNo].Possible {
+				colsForRow[rowNo][poss] = append(colsForRow[rowNo][poss], colNo)
 			}
 		}
+	}
 
-		for _, cells := range cellsWith {
-			if len(cells) == 2 {
-				// find out if any row has a possible value of cells[0].a or cells[0].b
-				// in columns cells[0].col and cells[1].col
-				a, b := cells[0].a, cells[0].b
-				colA, colB := cells[0].col, cells[1].col
-				for xrow := 0; xrow < 9; xrow++ {
-					if xrow == rowNo {
+	eliminatedCount := 0
+
+	for rowNo := 0; rowNo < 9; rowNo++ {
+		colsFor := colsForRow[rowNo]
+		for poss := 1; poss <= 9; poss++ {
+			if len(colsFor[poss]) == 2 {
+				for otherRow := 0; otherRow < 9; otherRow++ {
+					if rowNo == otherRow {
 						continue
 					}
-					if bd[xrow][colA].Solved || bd[xrow][colB].Solved {
-						continue
-					}
-					var possible [10]int
-					for _, poss := range bd[xrow][colA].Possible {
-						possible[poss]++
-					}
-					for _, poss := range bd[xrow][colB].Possible {
-						possible[poss]++
-					}
-
-					for i := 1; i <= 9; i++ {
-						if possible[i] == 2 {
-							if i == a {
-								fmt.Printf("can xwing %d in row %d, cols %d, %d\n",
-									a, xrow, colA, colB,
-								)
-							} else if i == b {
-								fmt.Printf("can xwing %d in row %d, cols %d, %d\n",
-									b, xrow, colA, colB,
+					otherCols := colsForRow[otherRow]
+					if len(otherCols[poss]) == 2 {
+						if (colsFor[poss][0] == otherCols[poss][0] && colsFor[poss][1] == otherCols[poss][1]) ||
+							(colsFor[poss][0] == otherCols[poss][1] && colsFor[poss][1] == otherCols[poss][0]) {
+							// Eliminate poss in colsFor[poss][0] and colsFor[poss][1]
+							if announce {
+								fmt.Printf(
+									"Xwing for %d, row <%d,%d>/<%d,%d>  <%d,%d>/<%d,%d>\n",
+									poss,
+									rowNo, colsFor[poss][0],
+									rowNo, colsFor[poss][1],
+									otherRow, otherCols[poss][0],
+									otherRow, otherCols[poss][1],
 								)
 							}
-							fmt.Printf("vs [%d,%d] in <%d,%d> <%d,%d>\n",
-								a, b,
-								cells[0].row, cells[0].col,
-								cells[1].row, cells[1].col,
-							)
+							colA, colB := colsFor[poss][0], colsFor[poss][1]
+							for newRow := 0; newRow < 9; newRow++ {
+								if newRow == rowNo || newRow == otherRow {
+									continue
+								}
+								m := bd.SpliceOut(newRow, colA, poss)
+								if m == 1 && announce {
+									fmt.Printf("Xwing eliminate %d at <%d,%d>\n", poss, newRow, colA)
+								}
+								eliminatedCount += m
+								m = bd.SpliceOut(newRow, colB, poss)
+								if m == 1 && announce {
+									fmt.Printf("Xwing eliminate %d at <%d,%d>\n", poss, newRow, colB)
+								}
+								eliminatedCount += m
+							}
 						}
 					}
 				}
@@ -66,5 +77,11 @@ func (bd *Board) XwingEliminate(announce bool) int {
 		}
 	}
 
+	return eliminatedCount
+}
+
+// xwingColEliminate finds 2 cols with some possible value in the same rows,
+// eliminates that possible value from the rows.
+func (bd *Board) xwingColEliminate(announce bool) int {
 	return 0
 }
