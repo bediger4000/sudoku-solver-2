47627e2cc6bdc86899a5e9ddd6d0a981bb18adf9
Bruce Ediger <bediger8@gmail.com>
2023-08-22 08:13:27 -0600

    hidden triplet elimination per row working

--- board/triplets.go
+++ board/triplets.go
@@ -245,12 +245,9 @@ func (bd *Board) HiddenTriplets(announce bool) int {
 }
 
 func (bd *Board) RowHiddenTriplets(announce bool) int {
-	fmt.Println("Enter RowHiddenTriplets")
-	defer fmt.Println("Exit RowHiddenTriplets")
 	candidatesEliminated := 0
 
 	for rowNo := 0; rowNo < 9; rowNo++ {
-		fmt.Printf("Examining row %d\n", rowNo)
 		bdRow := (*bd)[rowNo]
 		cellsPossible := make(map[int][]*Cell)
 		var count [10]int
@@ -260,7 +257,6 @@ func (bd *Board) RowHiddenTriplets(announce bool) int {
 				cellsPossible[v] = append(cellsPossible[v], &(bdRow[c]))
 			}
 		}
-		fmt.Printf("Values count %v\n", count)
 		var p2or3 []int
 		for i := 1; i < 10; i++ {
 			if count[i] == 2 || count[i] == 3 {
@@ -268,7 +264,6 @@ func (bd *Board) RowHiddenTriplets(announce bool) int {
 			}
 		}
 		ln := len(p2or3)
-		fmt.Printf("\t%d values appear 2 or 3 times: %v\n", ln, p2or3)
 		if ln < 3 {
 			if announce {
 				fmt.Printf("row %d does not have possible triplets\n", rowNo)
@@ -279,25 +274,19 @@ func (bd *Board) RowHiddenTriplets(announce bool) int {
 		// find out if they appear in only 3 cells
 		for i := 0; i < ln; i++ {
 			c1 := cellsPossible[p2or3[i]]
-			fmt.Printf("\tvalue 1 %d appears in %d cells\n", p2or3[i], len(c1))
 			if len(c1) < 2 || len(c1) > 3 {
 				continue
 			}
 			for j := i + 1; j < ln; j++ {
 				c2 := cellsPossible[p2or3[j]]
-				fmt.Printf("\tvalue 2 %d appears in %d cells\n", p2or3[j], len(c2))
 				if len(c2) < 2 || len(c2) > 3 {
 					continue
 				}
 				for k := j + 1; k < ln; k++ {
 					c3 := cellsPossible[p2or3[k]]
-					fmt.Printf("\tvalue 3 %d appears in %d cells\n", p2or3[k], len(c3))
 					if len(c3) < 2 || len(c3) > 3 {
 						continue
 					}
-					fmt.Printf("\t values %d,%d,%d all found in 3 cells\n",
-						p2or3[i], p2or3[j], p2or3[k],
-					)
 					// p2or3[i], p2or3[j], p2or3[k] appear in 3 cells.
 					// are they 3 and only 3 different cells?
 					all3cells := make(map[int]*Cell)
@@ -308,6 +297,7 @@ func (bd *Board) RowHiddenTriplets(announce bool) int {
 						}
 					}
 					if len(all3cells) == 3 {
+						// p2or3[i], p2or3[j], p2or3[k] appear in the correct 3 cells.
 						var tripletCells [3]*Cell
 						idx := 0
 						for _, cell := range all3cells {
@@ -315,14 +305,29 @@ func (bd *Board) RowHiddenTriplets(announce bool) int {
 							idx++
 						}
 						if announce {
-							fmt.Printf("triple <%d,%d,%d> all found in cells <%d,%d>, <%d,%d>, <%d,%d>\n",
+							fmt.Printf("triplet <%d,%d,%d> all found in cells <%d,%d>, <%d,%d>, <%d,%d>\n",
 								p2or3[i], p2or3[j], p2or3[k],
 								tripletCells[0].Row, tripletCells[0].Col,
 								tripletCells[1].Row, tripletCells[1].Col,
 								tripletCells[2].Row, tripletCells[2].Col,
 							)
 						}
-
+						// splice out all possible values except the
+						// values in p2or3, from the cells in tripletCells
+						for _, cell := range tripletCells {
+							for _, v := range cell.Possible {
+								if v == p2or3[i] ||
+									v == p2or3[j] ||
+									v == p2or3[k] {
+									continue
+								}
+								spliced := bd.SpliceOut(cell.Row, cell.Col, v)
+								if announce && spliced == 1 {
+									fmt.Printf("\teliminated %d at <%d,%d>\n", v, cell.Row, cell.Col)
+								}
+								candidatesEliminated += spliced
+							}
+						}
 					}
 				}
 			}
