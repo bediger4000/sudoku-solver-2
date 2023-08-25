6e8f8ba1fa1b07c590187b0b9275ecaf69b415db
Bruce Ediger <bediger8@gmail.com>
2023-08-25 14:31:37 -0600

    factor out all common code hidden triplets

--- board/triplets.go
+++ board/triplets.go
@@ -104,92 +104,110 @@ func (bd *Board) ColHiddenTriplets(announce bool) int {
 				cellsPossible[v] = append(cellsPossible[v], &((*bd)[r][colNo]))
 			}
 		}
-		var p2or3 []int
-		for i := 1; i < 10; i++ {
-			if count[i] == 2 || count[i] == 3 {
-				p2or3 = append(p2or3, i)
-			}
-		}
-		ln := len(p2or3)
-		if ln < 3 {
-			if announce {
-				fmt.Printf("column %d does not have possible triplets\n", colNo)
-			}
-			continue // colNo loop
-		}
-		// At least 3 possible values with 2 or 3 appearances each.
-		// find out if they appear in only 3 cells
-		for i := 0; i < ln; i++ {
-			c1 := cellsPossible[p2or3[i]]
-			if len(c1) < 2 || len(c1) > 3 {
-				continue
-			}
-			for j := i + 1; j < ln; j++ {
-				c2 := cellsPossible[p2or3[j]]
-				if len(c2) < 2 || len(c2) > 3 {
-					continue
-				}
-				for k := j + 1; k < ln; k++ {
-					c3 := cellsPossible[p2or3[k]]
-					if len(c3) < 2 || len(c3) > 3 {
-						continue
-					}
-					// all 3 values in p2or3 appear in exactly 3 cells, c1, c2, c3
-					candidatesEliminated += elimNonTriples(bd, c1, c2, c3, p2or3[i], p2or3[j], p2or3[k], announce)
-				}
-			}
-		}
+		candidatesEliminated += findTriplets(bd, count, cellsPossible, colNo, "column", announce)
 	}
 
 	return candidatesEliminated
 }
 
+// findTriplets looks through count and cellsPossible, finds triplets,
+// eliminates non-triplet possible values from the cells holding the triplets
+func findTriplets(bd *Board, count [10]int, cellsPossible map[int][]*Cell, number int, house string, announce bool) int {
+	candidatesEliminated := 0
+	// p2or3 holds all values in this "house" (row, col or block) that
+	// appear either 2 or 3 times.
+	var p2or3 []int
+	for i := 1; i < 10; i++ {
+		if count[i] == 2 || count[i] == 3 {
+			p2or3 = append(p2or3, i)
+		}
+	}
+	ln := len(p2or3)
+
+	if ln < 3 {
+		if announce {
+			fmt.Printf("%s %d does not have possible triplets\n", house, number)
+		}
+		return 0
+	}
+
+	// At least 3 possible values with 2 or 3 appearances each.
+	// find out if they appear in only 3 cells
+	for i := 0; i < ln; i++ {
+		c1 := cellsPossible[p2or3[i]]
+		if len(c1) < 2 || len(c1) > 3 {
+			// value p2or3[i] appears less than 2 or more than
+			// 3 times in this house
+			continue
+		}
+		for j := i + 1; j < ln; j++ {
+			c2 := cellsPossible[p2or3[j]]
+			if len(c2) < 2 || len(c2) > 3 {
+				continue
+			}
+			for k := j + 1; k < ln; k++ {
+				c3 := cellsPossible[p2or3[k]]
+				if len(c3) < 2 || len(c3) > 3 {
+					continue
+				}
+				// all 3 values in p2or3 appear in exactly 3 cells, c1, c2, c3
+				candidatesEliminated += elimNonTriples(bd, c1, c2, c3, p2or3[i], p2or3[j], p2or3[k], announce)
+			}
+		}
+	}
+	return candidatesEliminated
+}
+
+// elimNonTriples eliminates all values *other* that the triplet
+// p,q,r from the 3 cells c1, c2, c3 that contain them.
 func elimNonTriples(bd *Board, c1, c2, c3 []*Cell, p, q, r int, announce bool) int {
 	candidatesEliminated := 0
 
 	// p, q, r appear as possible values in 3 cells.
-	// Are they 3 and only 3 different cells?
-	all3cells := make(map[int]*Cell)
+	// Are those cells exactly 3 different cells?
+	allCells := make(map[int]*Cell)
+	var tripletCells []*Cell
 	for _, cls := range [][]*Cell{c1, c2, c3} {
 		for _, cell := range cls {
 			hash := 10*cell.Row + cell.Col
-			all3cells[hash] = cell
-		}
-	}
-	if len(all3cells) == 3 {
-		// p, q, r all appear in the correct 3 cells.
-		var tripletCells []*Cell
-		for _, cell := range all3cells {
+			allCells[hash] = cell
 			tripletCells = append(tripletCells, cell)
 		}
-		if announce {
-			fmt.Printf("triplet (%d,%d,%d) all found in cells <%d,%d>, <%d,%d>, <%d,%d>\n",
-				p, q, r,
-				tripletCells[0].Row, tripletCells[0].Col,
-				tripletCells[1].Row, tripletCells[1].Col,
-				tripletCells[2].Row, tripletCells[2].Col,
-			)
-		}
-		// splice out all possible values except the
-		// values in p2or3, from the cells in tripletCells
-		for _, cell := range tripletCells {
-			for idx := 0; idx < len(cell.Possible); {
-				v := cell.Possible[idx]
-				if v == p ||
-					v == q ||
-					v == r {
-					idx++
-					continue
-				}
-				if spliced := bd.SpliceOut(cell.Row, cell.Col, v); spliced > 0 {
-					if announce {
-						fmt.Printf("\teliminated %d at <%d,%d>\n", v, cell.Row, cell.Col)
-					}
-					candidatesEliminated += spliced
-					continue
-				}
+	}
+	if len(allCells) != 3 {
+		return 0
+	}
+
+	// p, q, r all appear in the correct 3 cells.
+
+	if announce {
+		fmt.Printf("triplet (%d,%d,%d) all found in cells <%d,%d>, <%d,%d>, <%d,%d>\n",
+			p, q, r,
+			tripletCells[0].Row, tripletCells[0].Col,
+			tripletCells[1].Row, tripletCells[1].Col,
+			tripletCells[2].Row, tripletCells[2].Col,
+		)
+	}
+
+	// splice out all possible values except the
+	// values in p2or3, from the cells in tripletCells
+	for _, cell := range tripletCells {
+		for idx := 0; idx < len(cell.Possible); {
+			v := cell.Possible[idx]
+			if v == p ||
+				v == q ||
+				v == r {
 				idx++
+				continue
 			}
+			if spliced := bd.SpliceOut(cell.Row, cell.Col, v); spliced > 0 {
+				if announce {
+					fmt.Printf("\teliminated %d at <%d,%d>\n", v, cell.Row, cell.Col)
+				}
+				candidatesEliminated += spliced
+				continue
+			}
+			idx++
 		}
 	}
 	return candidatesEliminated
@@ -203,10 +221,7 @@ func (bd *Board) BlockHiddenTriplets(announce bool) int {
 		var count [10]int
 		for r := 0; r < 9; r++ {
 			for c := 0; c < 9; c++ {
-				if (*bd)[r][c].Block != blockNo {
-					continue
-				}
-				if (*bd)[r][c].Solved {
+				if (*bd)[r][c].Block != blockNo || (*bd)[r][c].Solved {
 					continue
 				}
 				for _, v := range (*bd)[r][c].Possible {
@@ -215,41 +230,7 @@ func (bd *Board) BlockHiddenTriplets(announce bool) int {
 				}
 			}
 		}
-		var p2or3 []int
-		for i := 1; i < 10; i++ {
-			if count[i] == 2 || count[i] == 3 {
-				p2or3 = append(p2or3, i)
-			}
-		}
-		ln := len(p2or3)
-		if ln < 3 {
-			if announce {
-				fmt.Printf("block %d does not have possible triplets\n", blockNo)
-			}
-			continue // blockNo loop
-		}
-		// At least 3 possible values with 2 or 3 appearances each.
-		// find out if they appear in only 3 cells
-		for i := 0; i < ln; i++ {
-			c1 := cellsPossible[p2or3[i]]
-			if len(c1) < 2 || len(c1) > 3 {
-				continue
-			}
-			for j := i + 1; j < ln; j++ {
-				c2 := cellsPossible[p2or3[j]]
-				if len(c2) < 2 || len(c2) > 3 {
-					continue
-				}
-				for k := j + 1; k < ln; k++ {
-					c3 := cellsPossible[p2or3[k]]
-					if len(c3) < 2 || len(c3) > 3 {
-						continue
-					}
-					// all 3 values in p2or3 appear in exactly 3 cells, c1, c2, c3
-					candidatesEliminated += elimNonTriples(bd, c1, c2, c3, p2or3[i], p2or3[j], p2or3[k], announce)
-				}
-			}
-		}
+		candidatesEliminated += findTriplets(bd, count, cellsPossible, blockNo, "block", announce)
 	}
 
 	return candidatesEliminated
